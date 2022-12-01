f52fa306d6541cf38a54064493ebe4694294dc18
Yonyc <arnaudwery24@gmail.com>
2022-12-01 22:44:20 +0100

    Added scss

---
+++ modules/scss.js
@@ -0,0 +1,19 @@
+import { writeFile } from "fs";
+import { render } from "node-sass";
+
+render({
+    file: "./public/css/style.scss",
+    outFile: "./public/css/style.css",
+    outputStyle: "expanded"
+}, function (error, result) { // node-style callback from v3.0.0 onwards
+    if (error) {
+        console.error(error);
+        return;
+    }
+    // No errors during the compilation, write this result on the disk
+    writeFile("./public/css/style.css", result.css, function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
