2e73b5b9c3dab2ca273cd3eca35623787c64e9ea
Yonyc <arnaudwery24@gmail.com>
2022-12-01 23:15:30 +0100

    Initialized server and pages module

--- server.js
+++ server.js
@@ -1,2 +1,33 @@
-import {} from"./modules/database.js";
 import "./modules/scss.js";
+import * as db from "./modules/database.js";
+import pagesRouter from "./modules/pages.js";
+
+import express from "express";
+import session from "express-session";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+// Using ejs as render engine
+app.set('view engine', 'ejs');
+
+// Using post method
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Starting session 
+app.use(session({
+    secret: 'centretremplin',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 * 24 * 5 }
+}));
+
+app.use(express.static("public"));
+
+app.use(pagesRouter)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
