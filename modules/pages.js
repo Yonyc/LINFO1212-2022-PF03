7155454a8b7745e7c6eef5d111e979da726c84a3
Yonyc <arnaudwery24@gmail.com>
2022-12-05 19:54:42 +0100

    Added dynamic content change

--- modules/pages.js
+++ modules/pages.js
@@ -2,8 +2,30 @@ import { Router } from "express";
 
 const pagesRouter = new Router();
 
+function renderTemplate(req, res, path="", title="", args={}) {
+    if (req.query.content) {
+        res.render(path, args);
+        return;
+    }
+    if (req.query.infos) {
+        res.json({title: title});
+        return;
+    }
+    res.render('partials/template', {title: title, path: "../" + path, args: args });
+}
+
+pagesRouter.use("/profile", (req, res) => {
+    renderTemplate(req, res, "pages/profile", "CT - profil", {});
+});
+
+pagesRouter.use("/therapist", (req, res) => {});
+
+pagesRouter.use("/reservation", (req, res) => {});
+
+pagesRouter.use("/appointment", (req, res) => {});
+
 pagesRouter.use("/", (req, res) => {
-    res.render('partials/template', {title: "Centre tremplin", path: "../pages/index", args: {} });
+    renderTemplate(req, res, "pages/index", "Centre tremplin", {});
 });
 
 export default pagesRouter;
---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+
+
+function start() {
+
+    document.querySelectorAll("a").forEach(a => {
+        a.addEventListener("click", e => {
+            e.preventDefault();
+            if (!e.target.href) return;
+            fetchPage(e.target.href).then(page => {
+                changePage(page, e.target.href);
+            });
+        });
+    
+    });
+
+}
+
+function changeContentLinkEvent() {
+    document.querySelectorAll("body > .container-fluid a").forEach(a => {
+        a.addEventListener("click", e => {
+            e.preventDefault();
+            if (!e.target.href) return;
+            fetchPage(e.target.href).then(page => {
+                changePage(page, e.target.href);
+            });
+        });
+    
+    });
+}
+
+function changePage(data, urlPath){
+    document.querySelector("body .container-fluid").innerHTML = data.html;
+    document.title = data.pageTitle;
+    window.history.pushState({"html":data.html,"pageTitle":data.pageTitle},"", urlPath);
+    changeContentLinkEvent();
+}
+
+async function fetchPage(url) {
+    let page = fetch(url + "?content=true", {
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    let title = fetch(url + "?infos=true", {
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    page =  await (await page).text();
+    title = await (await title).json();
+    return {
+        html: page,
+        pageTitle: title.title
+    };
+}
+
+document.addEventListener("DOMContentLoaded", e => {
+    window.history.replaceState({"html":document.querySelector("body .container-fluid").innerHTML, "pageTitle":document.title}, '', document.location.href);
+    start();
+});
+
+window.onpopstate = function(e){
+    if(e.state){
+        document.querySelector("body > .container-fluid").innerHTML = e.state.html;
+        document.title = e.state.pageTitle;
+        changeContentLinkEvent();
+    }
+};
