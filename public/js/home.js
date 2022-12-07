0bc7e80027d03f7244cf8b1d33d2ea8a84f063e1
Yonyc <arnaudwery24@gmail.com>
2022-12-07 13:12:58 +0100

    Patched carousel breaking when changing page

--- public/js/home.js
+++ public/js/home.js
@@ -1,41 +1,45 @@
 /* Provide script for home page and for carousel slider */
 
-(function($) {
-	"use strict";
-	var fullHeight = function() {
 
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
+export function loadCarousel() {
+	(function ($) {
+		"use strict";
+		var fullHeight = function() {
+	
 			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-	var carousel = function() {
-		$('.home-slider').owlCarousel({
-			loop:true,
-			autoplay: true,
-			margin:0,
-			animateOut: 'fadeOut',
-			animateIn: 'fadeIn',
-			nav:true,
-			dots: true,
-			autoplayHoverPause: false,
-			items: 1,
-			navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
-			responsive:{
-				0:{
-					items:1
-				},
-				600:{
-					items:1
-				},
-				1000:{
-					items:1
+			$(window).resize(function(){
+				$('.js-fullheight').css('height', $(window).height());
+			});
+	
+		};
+		fullHeight();
+		var carousel = function() {
+			$('.home-slider').owlCarousel({
+				loop:true,
+				autoplay: true,
+				margin:0,
+				animateOut: 'fadeOut',
+				animateIn: 'fadeIn',
+				nav:true,
+				lazyLoad: true,
+				dots: true,
+				autoplayHoverPause: false,
+				items: 3,
+				navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
+				responsive:{
+					0:{
+						items:1
+					},
+					600:{
+						items:1
+					},
+					1000:{
+						items:1
+					}
 				}
-			}
-		});
-
-	};
-	carousel();
-})(jQuery);
+			});
+	
+		};
+		carousel();
+	})(jQuery);
+}
