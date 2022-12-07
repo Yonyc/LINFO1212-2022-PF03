ae333ceb65d1f1e5fa4bde052bd8914de0fe09dd
Yonyc <arnaudwery24@gmail.com>
2022-12-07 13:26:00 +0100

    Added script reload on profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,14 +4,24 @@ function updateText(selector, value) {
     });
 }
 
-updateText(".firstname", document.querySelector("#input_firstname").value);
+export function main() {
+    updateText(".firstname", document.querySelector("#input_firstname").value);
+    
+    updateText(".lastname", document.querySelector("#input_lastname").value);
+    
+    document.querySelector("#input_firstname").addEventListener("keyup", e => {
+        updateText(".firstname", e.target.value);
+    });
+    
+    document.querySelector("#input_lastname").addEventListener("keyup", e => {
+        updateText(".lastname", e.target.value);
+    });
 
-updateText(".lastname", document.querySelector("#input_lastname").value);
-
-document.querySelector("#input_firstname").addEventListener("keyup", e => {
-    updateText(".firstname", e.target.value);
-});
-
-document.querySelector("#input_lastname").addEventListener("keyup", e => {
-    updateText(".lastname", e.target.value);
-});
+    document.querySelectorAll("input").forEach(el => {
+        el.addEventListener("change", e => {
+            let a = document.querySelector("#profil_edit");
+            if (a)
+                a.removeAttribute("disabled");
+        });
+    });
+}
