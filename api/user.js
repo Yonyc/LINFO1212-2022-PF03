79f256b547dafbe2916c91d24307720943370cbb
Yonyc <arnaudwery24@gmail.com>
2022-12-07 22:35:32 +0100

    Patched url_pp in profile picture

--- api/user.js
+++ api/user.js
@@ -20,7 +20,8 @@ userApi.post("/login", (req, res) => {
             address: "Ici",
             website: "https://centre-tremplin.be",
             instagram: null,
-            facebook: null
+            facebook: null,
+            url_pp: "https://scontent-bru2-1.xx.fbcdn.net/v/t39.30808-6/241771405_1489509348094192_8935272199923345073_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=D0bbwGs9JeoAX_GIN-w&_nc_ht=scontent-bru2-1.xx&oh=00_AfDzgBm_TuY6uICoskWCENp5kzYI0sqQhPArEiCT295-xA&oe=6395AA30"
         }
     }); // Response example
 });
