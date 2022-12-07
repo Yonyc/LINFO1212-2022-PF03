02c2e0394c5f3bd8edaa00837c323c439dfaae43
RyuGood0 <ryugood0@gmail.com>
2022-12-07 22:39:57 +0100

    No reload register

--- api/user.js
+++ api/user.js
@@ -6,39 +6,118 @@ export const userApi = new Router();
     User have a token to send in the req.body for profile edit
 */
 
-userApi.post("/login", (req, res) => {
-    // req.body for POST request's body
-    res.json({
-        success: true,
-        data: {
-            username: "Yonyc",
-            firstname: "Arnaud",
-            lastname: "Wery",
-            email: "arnaudwery24@gmail.com",
-            mobile: "0469696969",
-            phone: null,
-            address: "Ici",
-            website: "https://centre-tremplin.be",
-            instagram: null,
-            facebook: null
-        }
-    }); // Response example
+import {User} from '../modules/database.js'
+import bcrypt from 'bcrypt'
+import passport from 'passport'
+import { Strategy as LocalStrategy } from "passport-local"
+
+const salt = "$2b$10$XzJlrKQwqwFg4DZNXmmHPO";
+
+async function getUser(username) {
+    return User.findOne({where: { username: username }});
+}
+
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        getUser(username)
+            .then(function (users) {
+                if (!users) {
+                    return done(null, false, { message: 'Incorrect username.' });
+                }
+                if (users.password == bcrypt.hashSync(password, salt)) {
+                  return done(null, users);
+                }
+                return done(null, false, { message: 'Incorrect password.' });
+            })
+            .catch(err => done(err));
+    }  
+));
+    
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username
+      });
+    });
+  });
+
+passport.deserializeUser(function(id, done) {
+  getUser(id.username).then(user => {
+    done(null, user);
+  }).catch(err => done(err));
 });
 
-userApi.post("/register", (req, res) => {
-    res.json({
-        success: false,
-        data: {
-            message: "Username is already taken"
+//=================================================================================================
+
+import bodyParser from 'body-parser'
+
+userApi.use((bodyParser.urlencoded({ extended: false })))
+userApi.use(bodyParser.json());
+
+userApi.post('/register', function(req,res){
+    let hashed = bcrypt.hashSync(req.body.password, salt);
+    var userData = [req.body.email, req.body.username, hashed, req.body.firstname, req.body.lastname, req.body.phone, req.body.mobile, req.body.address];
+    User.findOne({
+       where: { email: userData[0],
+        username: userData[1]
+       }
+
+    }).then(users => {
+        if (users) {
+            return res.status(200).send({
+                error: 'none'
+            });
         }
-    }); // Response example
+        else{
+            User.create({
+                email: userData[0],
+                username: userData[1],
+                password: userData[2],
+                firstname: userData[3],
+                lastname: userData[4],
+                phone: userData[5],
+                mobilephone: userData[6],
+                address: userData[7],
+                url_pp: "public/img/profile_pictures/photo-anonyme.png",
+                confirmed: false
+
+          }).then(users => {
+            return res.status(200).send({
+                error: 'none'
+            });
+          }).catch(function (reason) {
+            console.log("Account creation Rejected");
+            if (reason.errors[0].type == "Validation error" && reason.errors[0].path == "email") {
+                return res.status(400).send({
+                    error: 'email'
+                });
+            }
+       });    
+        }
+    }).catch(function (reason) {
+        console.log(reason);
+   });
+})
+
+userApi.post('/login', passport.authenticate('local', {}),
+function(req, res) {
+    let data = {
+      success: true,
+      user: {
+        username: req.user.username,
+        firstname: req.user.firstname,
+        lastname: req.user.lastname,
+        email: req.user.email,
+        phone: req.user.phone,
+        mobile: req.user.mobilephone,
+        address: req.user.address,
+      }
+    };
+    return res.status(200).send(data);
 });
 
-userApi.post("/edit", (req, res) => {
-    res.json({
-        success: false,
-        data: {
-            message: "Username is already taken"
-        }
-    }); // Response example
+userApi.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
 });
