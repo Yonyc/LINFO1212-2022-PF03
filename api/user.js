c48fc6860fc3c67be1753fff687bb9b2ef3ef753
Yonyc <arnaudwery24@gmail.com>
2022-12-09 12:02:24 +0100

    Added user login when reloading

--- api/user.js
+++ api/user.js
@@ -1,23 +1,15 @@
 import { Router } from "express";
+import { User } from '../modules/database.js';
+import bcrypt from 'bcrypt';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from "passport-local";
 
 export const userApi = new Router();
-
-/* 
-    User have a token to send in the req.body for profile edit
-*/
-
-import {User} from '../modules/database.js'
-import bcrypt from 'bcrypt'
-import passport from 'passport'
-import { Strategy as LocalStrategy } from "passport-local"
-
-userApi.use(passport.initialize());
-userApi.use(passport.session());
 
 const salt = "$2b$10$XzJlrKQwqwFg4DZNXmmHPO";
 
 async function getUser(username) {
-    return User.findOne({where: { username: username }});
+    return User.findOne({ where: { username: username } });
 }
 
 passport.use(new LocalStrategy(
@@ -28,56 +20,57 @@ passport.use(new LocalStrategy(
                     return done(null, false, { message: 'Incorrect username.' });
                 }
                 if (users.password == bcrypt.hashSync(password, salt)) {
-                  return done(null, users);
+                    return done(null, users);
                 }
                 return done(null, false, { message: 'Incorrect password.' });
             })
             .catch(err => done(err));
-    }  
+    }
 ));
-    
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, {
-        id: user.id,
-        username: user.username
-      });
-    });
-  });
 
-passport.deserializeUser(function(id, done) {
-  getUser(id.username).then(user => {
-    done(null, user);
-  }).catch(err => done(err));
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, {
+            id: user.id,
+            username: user.username
+        });
+    });
+});
+
+passport.deserializeUser(function (id, done) {
+    getUser(id.username).then(user => {
+        done(null, user);
+    }).catch(err => done(err));
 });
 
 //=================================================================================================
 
-userApi.post('/getallusers', function(req,res){
-  User.count()
-    .then(data => {
-        console.log(data)
-        return res.json(data);
-     })    
-      .catch(function (reason) {
-      console.log(reason);
- });
+userApi.post('/getallusers', function (req, res) {
+    User.count()
+        .then(data => {
+            console.log(data)
+            return res.json(data);
+        })
+        .catch(function (reason) {
+            console.log(reason);
+        });
 })
 
-userApi.post('/register', function(req,res){
+userApi.post('/register', function (req, res) {
     let hashed = bcrypt.hashSync(req.body.password, salt);
     var userData = [req.body.email, req.body.username, hashed, req.body.firstname, req.body.lastname, req.body.phone, req.body.mobile, req.body.address];
     userData.forEach(element => {
-      if (element == "") {
-        return res.status(400).send({
-          error: 'empty'
-      });
-      }
+        if (element == "") {
+            return res.status(400).send({
+                error: 'empty'
+            });
+        }
     });
     User.findOne({
-       where: { email: userData[0],
-        username: userData[1]
-       }
+        where: {
+            email: userData[0],
+            username: userData[1]
+        }
 
     }).then(users => {
         if (users) {
@@ -85,7 +78,7 @@ userApi.post('/register', function(req,res){
                 error: 'none'
             });
         }
-        else{
+        else {
             User.create({
                 email: userData[0],
                 username: userData[1],
@@ -98,90 +91,90 @@ userApi.post('/register', function(req,res){
                 url_pp: "public/img/profile_pictures/photo-anonyme.png",
                 confirmed: false
 
-          }).then(users => {
-            return res.status(200).send({
-                error: 'none'
-            });
-          }).catch(function (reason) {
-            console.log("Account creation Rejected");
-            if (reason.errors[0].type == "Validation error" && reason.errors[0].path == "email") {
-                return res.status(400).send({
-                    error: 'email'
+            }).then(users => {
+                return res.status(200).send({
+                    error: 'none'
                 });
-            }
-       });    
+            }).catch(function (reason) {
+                console.log("Account creation Rejected");
+                if (reason.errors[0].type == "Validation error" && reason.errors[0].path == "email") {
+                    return res.status(400).send({
+                        error: 'email'
+                    });
+                }
+            });
         }
     }).catch(function (reason) {
         console.log(reason);
-   });
+    });
 })
 
 userApi.post('/login', passport.authenticate('local', {}),
-function(req, res) {
-    let data = {
-      success: true,
-      user: {
-        username: req.user.username,
-        firstname: req.user.firstname,
-        lastname: req.user.lastname,
-        email: req.user.email,
-        phone: req.user.phone,
-        mobile: req.user.mobilephone,
-        address: req.user.address,
-      }
-    };
-    return res.status(200).send(data);
-});
+    function (req, res) {
+        let data = {
+            success: true,
+            user: {
+                username: req.user.username,
+                firstname: req.user.firstname,
+                lastname: req.user.lastname,
+                email: req.user.email,
+                phone: req.user.phone,
+                mobile: req.user.mobilephone,
+                address: req.user.address,
+            }
+        };
+        return res.status(200).send(data);
+    });
 
-userApi.get('/logout', function(req, res){
+userApi.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
 });
 
-userApi.post('/edit', async function(req,res){
-  var userData = [req.body.email, req.body.username, req.body.firstname, req.body.lastname, req.body.phone, req.body.mobile, req.body.address];
-  userData.forEach(element => {
-    if (element == "") {
-      return res.status(400).send({
-        error: 'empty'
-      });
-    }
-  });
-  
-  await User.update(
-    {
-      email: userData[0],
-      username: userData[1],
-      firstname: userData[2],
-      lastname: userData[3],
-      phone: userData[4],
-      mobilephone: userData[5],
-      address: userData[6],
-    },
-    {
-      where: { username: req.user.username },
-    }
-  );
-
-  const updatedUser = req.user;
-  updatedUser.email= userData[0];
-  updatedUser.username= userData[1];
-  updatedUser.firstname= userData[2];
-  updatedUser.lastname= userData[3];
-  updatedUser.phone= userData[4];
-  updatedUser.mobilephone= userData[5];
-  updatedUser.address= userData[6];
-
-
-  req.login(updatedUser, async(error) => {
-    if (error) {
-      return res.status(400).send({
-        error: 'updateError'
-      });
-    }
-
-    return res.status(200).send({
-      error: 'none'
+userApi.post('/edit', async function (req, res) {
+    var userData = [req.body.email, req.body.username, req.body.firstname, req.body.lastname, req.body.phone, req.body.mobile, req.body.address];
+    userData.forEach(element => {
+        if (element == "") {
+            return res.status(400).send({
+                error: 'empty'
+            });
+        }
     });
-  });
+
+    await User.update(
+        {
+            email: userData[0],
+            username: userData[1],
+            firstname: userData[2],
+            lastname: userData[3],
+            phone: userData[4],
+            mobilephone: userData[5],
+            address: userData[6],
+        },
+        {
+            where: { username: req.user.username },
+        }
+    );
+
+    const updatedUser = req.user;
+    updatedUser.email = userData[0];
+    updatedUser.username = userData[1];
+    updatedUser.firstname = userData[2];
+    updatedUser.lastname = userData[3];
+    updatedUser.phone = userData[4];
+    updatedUser.mobilephone = userData[5];
+    updatedUser.address = userData[6];
+
+
+    req.login(updatedUser, async (error) => {
+        if (error) {
+            return res.status(400).send({
+                error: 'updateError'
+            });
+        }
+
+        return res.status(200).send({
+            error: 'none'
+        });
+    });
 })
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import {renderCSS} from "./modules/scss.js";
 import * as db from "./modules/database.js";
 import {pagesRouter} from "./modules/pages.js";
-
-import express from "express";
 import session from "express-session";
+import express from "express";
 import bodyParser from "body-parser";
 import { api } from "./api/api.js";
+import passport from "passport";
 
 renderCSS();
 const app = express();
@@ -17,7 +17,6 @@ app.set('view engine', 'ejs');
 // Using post method
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 // Starting session 
 app.use(session({
     secret: 'centretremplin',
@@ -25,6 +24,9 @@ app.use(session({
     saveUninitialized: true,
     cookie: { maxAge: 60 * 60 * 1000 * 24 * 5 }
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static("public"));
 
