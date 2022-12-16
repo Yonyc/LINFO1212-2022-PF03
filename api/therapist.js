b766a438da2b87ea30e6bb7e150dd22f3faf2a07
Yonyc <arnaudwery24@gmail.com>
2022-12-16 17:29:45 +0100

    Added refuse therapist option

--- api/therapist.js
+++ api/therapist.js
@@ -5,7 +5,7 @@ export const therapistApi = new Router();
 import {Therapist} from '../modules/database.js'
 
 therapistApi.post('/getalltherapist', function(req,res){
-    Therapist.count()
+    Therapist.count({ where: { approved: true } })
     .then(data => {
         return res.json(data);
     })    
