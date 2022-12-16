15f01ea5bab1de968a3d149cb01bbef662eb6bb4
Yonyc <arnaudwery24@gmail.com>
2022-12-16 13:21:40 +0100

    Created base admin therapist verification

--- modules/adminPages.js
+++ modules/adminPages.js
@@ -2,9 +2,22 @@ import { Router } from "express";
 
 export const adminPagesRouter = new Router();
 
+function renderTemplate(req, res, path="", title="", args={}) {
+    if (req.query.content) {
+        res.render(path, args);
+        return;
+    }
+    if (req.query.infos) {
+        res.json({title: title});
+        return;
+    }
+
+    res.render("partials/template", {title: title, path: "../" + path, args: args });
+}
+
 adminPagesRouter.use("/", (req, res, next) => {
     if (!req.user || !req.user.admin) {
-        res.status(401);
+        res.status(401).send("You are not administrator");
         res.end();
         return;
     }
@@ -12,7 +25,7 @@ adminPagesRouter.use("/", (req, res, next) => {
 });
 
 adminPagesRouter.use("/therapist_demands", (req, res) => {
-    
+    renderTemplate(req, res, "admin/therapist_demands", "CT - Therapists Demands");
 });
 
 adminPagesRouter.use("/users", (req, res) => {
