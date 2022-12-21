b473c62b25003ac38229ed591467b0e8efe95e69
JarodRoss <jarodross852@gmail.com>
2022-12-21 18:00:42 +0100

    Fix path and template for admin section

--- modules/adminPages.js
+++ modules/adminPages.js
@@ -20,8 +20,20 @@ adminPagesRouter.use("/", async (req, res, next) => {
     next();
 });
 
+adminPagesRouter.use("/panel", (req, res) => {
+    renderTemplate(req, res, "admin/panel", "CT - Panel Admin");
+});
+
+adminPagesRouter.use("/home", (req, res) => {
+    renderTemplate(req, res, "admin/home", "CT - Panel Admin");
+});
+
 adminPagesRouter.use("/therapists", (req, res) => {
     renderTemplate(req, res, "admin/therapists", "CT - Therapists Demands");
+});
+
+adminPagesRouter.use("/rooms", (req, res) => {
+    renderTemplate(req, res, "admin/rooms", "CT - Admin - Rooms Managment");
 });
 
 adminPagesRouter.use("/users", (req, res) => {
