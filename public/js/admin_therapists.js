4f4380c19c70e946a650e3f38e120529a2f84eac
Yonyc <arnaudwery24@gmail.com>
2022-12-22 17:27:21 +0100

    Changed approve therapist view

--- public/js/admin_therapists.js
+++ public/js/admin_therapists.js
@@ -1,31 +1,44 @@
 async function reloadTherapistList() {
-    let list_container = document.querySelector(".demands-list");
+    let list_container = document.querySelector(".therapists_demands");
     if (!list_container) return;
 
-    let therapist_demands = await fetch(api_url + "/admin/therapist/approvals", {method: "POST"});
+    let therapist_demands = await fetch(api_url + "/admin/therapist/approvals", { method: "POST" });
     therapist_demands = await therapist_demands.json();
 
     if (!therapist_demands.success) return;
 
     list_container.innerHTML = "";
     therapist_demands.therapists.forEach(therapist => {
-        let card = document.createElement('div');
-        card.classList.add("card");
-        card.style.width = "25rem";
+        let card = document.createElement('li');
+        card.classList.add("position-relative");
+        card.classList.add("booking");
 
-        let cb = document.createElement('div');
-        cb.classList.add("card-body");
-        cb.classList.add("d-flex");
-        cb.classList.add("justify-content-evenly");
+        card.innerHTML += `<div class="media">
 
-        let ct = document.createElement('h5');
-        ct.classList.add("card-title");
-        ct.innerText = `${therapist.User?.firstname} ${therapist.User?.lastname}`;
+            <div class="msg-img">
+                <img class="profile_picture" alt="" src="${therapist.User.url_pp}">
+            </div>
+
+            <div class="media-body">
+                <h5 class="mb-4">${therapist.User.firstname} ${therapist.User.lastname}</h5>
+                <div class="mb-5">
+                    <span class="mr-2 d-block d-sm-inline-block mb-1 mb-sm-0">Détail de l'utilisateur:</span>
+                    <span class="border-right pr-2 mr-2">${therapist.User.username}</span>
+                    <span class="border-right pr-2 mr-2">${therapist.User.email}</span>
+                    <span class="phone">${therapist.User.phone}</span>
+                </div>
+            </div>
+
+        </div>`;
+
+        let btnContainer = document.createElement("div");
+        btnContainer.classList.add("buttons-to-right");
 
         let approveBtn = document.createElement("btn");
         approveBtn.classList.add("btn");
-        approveBtn.classList.add("btn-outline-success");
-        approveBtn.innerText = "Promote";
+        approveBtn.classList.add("btn-gray");
+        approveBtn.classList.add("mr-2");
+        approveBtn.innerHTML = `<i class="far fa-check-circle mr-2"></i>Approuver`;
         approveBtn.addEventListener("click", async e => {
             await fetch(api_url + "/admin/therapist/promote", {
                 method: "POST",
@@ -38,11 +51,11 @@ async function reloadTherapistList() {
             });
             reloadTherapistList();
         });
-        
+
         let refuseBtn = document.createElement("btn");
         refuseBtn.classList.add("btn");
-        refuseBtn.classList.add("btn-outline-danger");
-        refuseBtn.innerText = "Refuse";
+        refuseBtn.classList.add("btn-gray");
+        refuseBtn.innerHTML = `<i class="far fa-times-circle mr-2"></i> Refuser`;
         refuseBtn.addEventListener("click", async e => {
             await fetch(api_url + "/admin/therapist/refuse", {
                 method: "POST",
@@ -55,11 +68,9 @@ async function reloadTherapistList() {
             });
             reloadTherapistList();
         });
-
-        cb.appendChild(ct);
-        cb.appendChild(approveBtn);
-        cb.appendChild(refuseBtn);
-        card.appendChild(cb);
+        btnContainer.appendChild(approveBtn);
+        btnContainer.appendChild(refuseBtn);
+        card.appendChild(btnContainer);
 
         list_container.appendChild(card);
     });
